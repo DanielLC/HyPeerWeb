dac833508898f5bc52c71eaab827049df1314221
DanielLC <daniel_l_c@yahoo.com>
2013-05-06 20:40:19 -0600

    Worked on Node, and added a question to the Readme.

--- HyPeerWeb/src/dbPhase/hypeerweb/Node.java
+++ HyPeerWeb/src/dbPhase/hypeerweb/Node.java
@@ -1,23 +1,81 @@
 package dbPhase.hypeerweb;
 
+import java.util.HashSet;
+
 public class Node {
+	private WebId id;
+	private HashSet<Node> neighbors;
+	private HashSet<Node> upPointers;
+	private HashSet<Node> downPointers;
+	private Node fold;
+	private Node surrogateFold;
+	private Node inverseSurrogateFold;
 	
-	public static final Node NULL_NODE;
+	/*public static final Node NULL_NODE = new Node() {	//Why did it ask for two curly braces?
+		public Node() {
+			id = WebId.NULL_WEB_ID;
+			neighbors = new HashSet<Node>();
+			upPointers = new HashSet<Node>();
+			downPointers = new HashSet<Node>();
+			fold = NULL_NODE;
+			surrogateFold = NULL_NODE;
+			inverseSurrogateFold = NULL_NODE;
+		}
+	};*/
+	
+	public static final Node NULL_NODE = new Node(WebId.NULL_WEB_ID);
+	
+	//private Node(){};//I seem to need this to make the inner class work.
+	
+	public Node(WebId id) {	//I'm just using this for the NULL_NODE, but it could be useful.
+		this.id = id;
+		neighbors = new HashSet<Node>();
+		upPointers = new HashSet<Node>();
+		downPointers = new HashSet<Node>();
+		fold = NULL_NODE;
+		surrogateFold = NULL_NODE;
+		inverseSurrogateFold = NULL_NODE;
+	}
 	
 	public Node(int id) {
-		
+		this.id = new WebId(id);
+		neighbors = new HashSet<Node>();
+		upPointers = new HashSet<Node>();
+		downPointers = new HashSet<Node>();
+		fold = NULL_NODE;
+		surrogateFold = NULL_NODE;
+		inverseSurrogateFold = NULL_NODE;
 	}
 	
 	public Node(int id, int height) {
-		
+		this.id = new WebId(id, height);
+		neighbors = new HashSet<Node>();
+		upPointers = new HashSet<Node>();
+		downPointers = new HashSet<Node>();
+		fold = NULL_NODE;
+		surrogateFold = NULL_NODE;
+		inverseSurrogateFold = NULL_NODE;
 	}
 	
 	public SimplifiedNodeDomain constructSimplifiedNodeDomain() {
-		
+	    HashSet<Integer> intNeighbors = new HashSet<Integer>();
+	    for(Node neighbor : neighbors) {
+	    	intNeighbors.add(neighbor.getId());
+	    }
+	    HashSet<Integer> intUpPointers = new HashSet<Integer>();
+	    for(Node upPointer : upPointers) {
+	    	intUpPointers.add(upPointer.getId());
+	    }
+	    HashSet<Integer> intDownPointers = new HashSet<Integer>();
+	    for(Node downPointer : downPointers) {
+	    	intDownPointers.add(downPointer.getId());
+	    }
+	    return new SimplifiedNodeDomain(this.getId(), id.getHeight(), intNeighbors, intUpPointers, intDownPointers,
+	    		fold.getId(), surrogateFold.getId(), inverseSurrogateFold.getId());
 	}
 	
 	public WebId getWebId() {
-		
+		return id;
 	}
 	
 	public dbPhase.spec.Connections getConnections(){
@@ -25,7 +83,7 @@ public class Node {
 	}
 	
 	public void setWebId(WebId webId) {
-		
+		id = webId;
 	}
 	
 	public void setConnections(dbPhase.spec.Connections connections) {
@@ -33,39 +91,42 @@ public class Node {
 	}
 	
 	public void addNeighbor(Node neighbor) {
-		
+		neighbors.add(neighbor);
 	}
 	
 	public void removeNeighbor(Node neighbor) {
-		
+		neighbors.remove(neighbor);
 	}
 	
 	public void addUpPointer(Node upPointer) {
-		
+		upPointers.add(upPointer);
 	}
 	
 	public void removeUpPointer(Node upPointer) {
-		
+		upPointers.remove(upPointer);
 	}
 	
 	public void addDownPointer(Node downPointer) {
-		
+		downPointers.add(downPointer);
 	}
 	
 	public void removeDownPointer(Node downPointer) {
-		
+		downPointers.remove(downPointer);
 	}
 	
 	public void setFold(Node newFold) {
-		
+		fold = newFold;
 	}
 	
 	public void setSurrogateFold(Node newSurrogateFold) {
-		
+		surrogateFold = newSurrogateFold;
 	}
 	
 	public void setInverseSurrogateFold(Node newInverseSurrogateFold) {
-		
+		inverseSurrogateFold = newInverseSurrogateFold;
 	}
 
+	private int getId() {
+		return id.getValue();
+	}
 }
--- HyPeerWeb/src/readme.txt
+++ HyPeerWeb/src/readme.txt
@@ -1,1 +1,3 @@
 Our Hyper Web
+
+What is dbPhase.spec.Connections? Whoever wrote the outline of Node used it, but that package doesn't seem to exist. --Daniel Carrier
