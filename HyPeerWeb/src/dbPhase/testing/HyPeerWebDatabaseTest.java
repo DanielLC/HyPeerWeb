fb456439098e7d501d8a73e496cc67f80ee17ab6
Jacob Gillespie <jacobgillespie@gmail.com>
2013-05-08 12:05:44 -0600

    Added empty JUnit classes.

---
+++ HyPeerWeb/src/dbPhase/testing/HyPeerWebDatabaseTest.java
@@ -0,0 +1,14 @@
+package dbPhase.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HyPeerWebDatabaseTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
