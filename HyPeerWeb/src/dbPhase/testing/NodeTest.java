d9458642a49f37530177a2c7df6608fa6cc05706
Jacob Gillespie <jacobgillespie@gmail.com>
2013-05-08 12:29:07 -0600

    Added JUnit test for Node class.

--- HyPeerWeb/src/dbPhase/testing/NodeTest.java
+++ HyPeerWeb/src/dbPhase/testing/NodeTest.java
@@ -4,11 +4,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class NodeTest {
+import dbPhase.hypeerweb.Node;
+import dbPhase.hypeerweb.WebId;
 
+public class NodeTest
+{
+	private Node node = new Node(0);
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetSetWebId() 
+	{
+		assertNotNull("Checking if the node isn't null.",node.getWebId());
+		
+		WebId webId = new WebId(1);
+		node.setWebId(webId);
+		assertSame("Checking if the set webId was stored", webId, node.getWebId());
 	}
+	
+	
 
 }
